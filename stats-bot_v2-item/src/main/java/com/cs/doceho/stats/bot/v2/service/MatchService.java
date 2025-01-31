e8bf45f372ea41dd8cd2e879e16c668cc07c6d3f
Alexander <alekspavlov02@mail.ru>
2025-01-31 12:43:15 +0400

    Ticket-26 add TopService and MatchService

--- stats-bot_v2-item/src/main/java/com/cs/doceho/stats/bot/v2/service/MatchService.java
+++ stats-bot_v2-item/src/main/java/com/cs/doceho/stats/bot/v2/service/MatchService.java
@@ -10,13 +10,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import javax.transaction.Transactional;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class MatchService {
 
   MatchRepository matchRepository;
--- stats-bot_v2-item/src/main/java/com/cs/doceho/stats/bot/v2/service/TopService.java
+++ stats-bot_v2-item/src/main/java/com/cs/doceho/stats/bot/v2/service/TopService.java
@@ -9,13 +9,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.transaction.Transactional;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class TopService {
 
   TopRepository topRepository;
